4d91f5ede2cd372d788e0eea27ec7c67ece64dcf
Yohannes Fikre <yohannesfk123@gmail.com>
2025-08-30 18:48:34 +0300

    Add hello API

---
+++ api/contact.js
@@ -0,0 +1,36 @@
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { name, email, message } = req.body;
+
+    if (!name || !email || !message) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // Use your Vercel .env credentials
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASS, // App password if 2FA
+      },
+    });
+
+    try {
+      await transporter.sendMail({
+        from: email,
+        to: process.env.NODEMAILER_USER,
+        subject: `New message from ${name}`,
+        text: message,
+      });
+
+      return res.status(200).json({ message: "Message sent successfully" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Email failed" });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
