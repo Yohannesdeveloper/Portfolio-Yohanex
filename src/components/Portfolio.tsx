bd0cd3165a6f8fe24b67297e77834b3f1f667701
Yohannes Fikre <yohannesfk123@gmail.com>
2026-02-04 15:26:11 +0300

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -49,7 +49,7 @@ const projects = [
     description:
       "HustleX is a platform that helps freelancers and hustlers automate tasks, grow communities, and manage projects efficiently via web, mobile apps, and Telegram bots.",
     image: "/Images/Screenshot 2026-01-19 232920.png",
-    link: "https://cultural-website-beta.vercel.app/",
+    link: "https://hustle-x-freelancing-platform-p6bi-hoaro2fkl.vercel.app/",
   },
 ];
 
